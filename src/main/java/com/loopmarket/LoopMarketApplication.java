33a9fd77536441400c5ac8c6cf1cb5a080f6087e
fort4 <adcretary@gmail.com>
2025-05-19 12:36:20 +0900

    Initial commit

---
+++ src/main/java/com/loopmarket/LoopMarketApplication.java
@@ -0,0 +1,13 @@
+package com.loopmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loopmarket/LoopMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
