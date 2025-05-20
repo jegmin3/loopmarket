44cc6f3b8e92f810a192931afdaaef2baf379b7b
fort4 <adcretary@gmail.com>
2025-05-20 17:14:04 +0900

    render 함수(java) 추가

---
+++ src/main/java/com/loopmarket/common/controller/LayoutController.java
@@ -0,0 +1,19 @@
+package com.loopmarket.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LayoutController extends BaseController {
+	
+	// http://localhost:8080/
+    @GetMapping("/")
+    public String index(Model model) {
+        return render("main/index", model);
+    }
+
+    
+    
+}
+
