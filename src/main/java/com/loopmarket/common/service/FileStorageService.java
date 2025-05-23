c3a5321f6786b2d27be09abae71564bb626e06d9
jegmin3 <lunate0322@gmail.com>
2025-05-23 10:27:19 +0900

    상품 등록에 AWS S3 이미지 업로드 연동

--- src/main/java/com/loopmarket/common/service/FileStorageService.java
+++ src/main/java/com/loopmarket/common/service/FileStorageService.java
@@ -1,4 +1,4 @@
-package com.loopmarket.common.util;
+package com.loopmarket.common.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
